fa28c503f90cefd58c6452e3ff0639a7930a1d85
Joey Sabani <axhi12@gmail.com>
2017-11-06 16:41:23 -0600

    [#152546512] Main nyp brand

--- app/src/main/App.js
+++ app/src/main/App.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
-import './App.css';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Not Your Pipeline</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="app-main">Coming Soon, I guess.</p>
       </div>
     );
   }

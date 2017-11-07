d91bc09b91fe28398319939390af705f6ef55895
Joey Sabani <axhi12@gmail.com>
2017-11-07 09:01:47 -0600

    [#152546523] Prod pipeline access

--- concourse/tasks/build-app.sh
+++ concourse/tasks/build-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd src/app && npm install && npm run-script build
-mv manifest.yml ../../build-output
+mv manifest* ../../build-output
 mv Staticfile ../../build-output
 mv build ../../build-output

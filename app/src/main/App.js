7a9d24c7822f1bdb7c36940c822692db9e0f3282
Joey Sabani <axhi12@gmail.com>
2017-11-07 11:18:06 -0600

    [#152546537] Two fixed panes for nyp pipeline

--- app/src/main/App.js
+++ app/src/main/App.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
+import Iframe from 'react-iframe';
 import './app.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <header className="app-header">
-          <h1 className="app-title">Not Your Pipeline</h1>
-        </header>
-        <p className="app-main">Coming Soon, I guess.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <div className='box-element'>
+                    <Iframe url="http://54.174.113.230:8080/teams/main/pipelines/not-your-pipeline?groups=dev"
+                            height="100%"
+                            position="relative"/>
+                </div>
+                <div className='box-element'>
+                    <Iframe url="http://54.174.113.230:8080/teams/main/pipelines/not-your-pipeline?groups=prod"
+                            position="relative"/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- app/src/main/__tests__/App.test.js
+++ app/src/main/__tests__/App.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from "../App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('App()', () => {
+    const component = shallow(<App/>);
+
+    it('sets the component up', () => {
+        const divs = component.find("div");
+
+        expect(divs.length).toBeGreaterThan(0);
+    });
+
+    it('builds two divs side by each', () => {
+        const eachDivs = component.find(".box-element");
+
+        expect(eachDivs.length).toBe(2);
+    });
+
+    it('builds iframes inside of each existing div', () => {
+        const iframes = component.find("Iframe");
+
+        expect(iframes.length).toBe(2);
+    });
+
+    it('hardcodes pipeline endpoints', () => {
+        const iframes = component.find("Iframe");
+
+        expect(iframes.first().props().url).toContain("not-your-pipeline")
+        expect(iframes.last().props().url).toContain("not-your-pipeline")
+    });
 });

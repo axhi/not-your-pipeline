96d2e836f8ea47581ce1327f2ec1a5ef8b2f5c87
Joey Sabani <axhi12@gmail.com>
2017-11-07 08:55:44 -0600

    [#152546512] Only move the folders for build

--- concourse/tasks/build-app.sh
+++ concourse/tasks/build-app.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cd src/app && npm install && npm build && mv * ../../build-output/
+cd src/app && npm install && npm run-script build
+mv manifest.yml ../../build-output
+mv Staticfile ../../build-output
+mv build ../../build-output

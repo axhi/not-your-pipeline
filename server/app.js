de40c4169786263f3227cb0293efa17d7ab0aef3
Joey Sabani <axhi12@gmail.com>
2017-11-08 16:14:02 -0600

    [#152698691] Build out new server

---
+++ server/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const url = require('url');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+const router = express.Router();
+
+function goGetUrl(req, res) {
+    const url_parts = url.parse(req.url, true);
+    const query = url_parts.query;
+    const urlGo = query.go;
+
+    if (!urlGo) {
+        return res.json({error: 'Need a GO path'});
+    }
+    const urlToParse = url.parse(urlGo, true);
+
+    return {
+        url: urlToParse.protocol + "//" + urlToParse.host + '/api/v1' + urlToParse.pathname + "/jobs",
+        group: urlToParse.query.groups
+    };
+}
+
+function renderResponse(res, body, group) {
+    let b = JSON.parse(body);
+    if (group) {
+        b = b.filter(function(ob) {
+            return ob.groups.indexOf(group) > -1;
+        });
+    }
+
+    return res.json(b);
+}
+
+router.get('/url', function (req, res) {
+    const urlToUse = goGetUrl(req, res);
+
+    return request(urlToUse, function (error, response, body) {
+        if (!error) {
+            renderResponse(res, body, urlToUse.group);
+        } else {
+            res.json({error: error.toString()})
+        }
+    });
+});
+
+app.use('/', router);
+app.listen(port);

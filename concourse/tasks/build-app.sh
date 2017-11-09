f7c5b3e680712de3e67f4f95d87ce6e8e22318aa
Joey Sabani <axhi12@gmail.com>
2017-11-09 08:41:32 -0600

    [#152546564] Update a few dependencies and run commands

--- concourse/tasks/build-app.sh
+++ concourse/tasks/build-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd src/app && npm install && npm run-script build
+cd src/app && npm install && npm run-script $($RUN)
 mv manifest* ../../build-output
 mv Staticfile ../../build-output
 mv build ../../build-output
